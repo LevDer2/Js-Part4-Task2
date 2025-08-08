61ba4a089ec249edaf4c4a04e9b2df1c19793de6
LevDer2 <123574272+LevDer2@users.noreply.github.com>
2025-08-08 12:36:43 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -9,14 +9,11 @@ function func(event) {
 
   const box = document.getElementById('box');
 
-  // Функція переміщення
   function moveBox(e) {
     box.style.left = e.clientX + 'px';
     box.style.top = e.clientY + 'px';
   }
 
-  // Використовуємо debounce з lodash (100 мс)
   const debouncedMove = _.debounce(moveBox, 100);
 
-  // Вішаємо на подію миші
   document.addEventListener('mousemove', debouncedMove);
